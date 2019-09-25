95ef60f0334b4dbe0b91bc74d7b8e887faa37ced
Moocow9m <devmoocow9m@gmail.com>
2019-09-25 08:47:00 -0700

    Start grabbing Viken.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.40"
+    idea
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+    kotlin("jvm") version "1.3.50"
 }
 
 group = "com.wavProductions.www"
@@ -9,13 +12,37 @@ version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://www.jitpack.io")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+    implementation("org.msgpack", "msgpack", "0.6.12") //TODO implement and replace with up to date kotlin version
+    implementation("com.github.Moocow9m:Viken:master-SNAPSHOT")
+    testImplementation("junit", "junit", "4.12")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("armitage")
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "com.wavproductions.www.armitage.MainKt"))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
--- src/main/kotlin/com/wavProductions/www/armitage/Main.kt
+++ src/main/kotlin/com/wavProductions/www/armitage/Main.kt
@@ -1,5 +1,17 @@
-package com.wavProductions.www.armitage
+package com.wavproductions.www.armitage
+
+import com.wavproductions.viken.Viken
+import com.wavproductions.www.armitage.Config.loadConfig
 
 fun main() {
-
+    loadConfig()
+    var conn = Metasploit()
+    Viken.init()
+    Viken.createSync(800, 800, "Armitage")
+    Viken.loop()
+    while (!conn.consoleActive()) {
+        Thread.onSpinWait()
+        Thread.sleep(20000) //run for 20 seconds for testing
+    }
+    Viken.cleanup()
 }
